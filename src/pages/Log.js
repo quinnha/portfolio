fe18ca4a42be55c90c18496ea88cf623814aed27
quinnha <cadetha@gmail.com>
2023-03-12 00:47:19 -0500

    fixed logbook title

--- src/pages/Log.js
+++ src/pages/Log.js
@@ -4,7 +4,7 @@ import BlogNavBar from "../components/blog_navbar";
 
 function Log() {
   useEffect(() => {
-    document.title = "quinn's life (〃 ω 〃)";
+    document.title = "quinn's log (/ω＼)";
   }, []);
 
   return (
