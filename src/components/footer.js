dcd3f38d4725e842cd436790133b860ddd99fc5d
quinnha <cadetha@gmail.com>
2023-08-12 22:07:52 -0400

    removed links from navbar

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,38 +24,7 @@ function Footer() {
         >
           {" "}
         </a>
-        <a
-          href="https://www.instagram.com/qh.music/"
-          target="_blank"
-          class="fa fa-instagram fa-lg"
-        >
-          {" "}
-        </a>
-        <a
-          href="https://sesamebagel.substack.com/"
-          target="_blank"
-          class="fa fa-bookmark fa-lg"
-        >
-          {" "}
-        </a>
-        <a
-          href="https://bento.me/quinnha"
-          target="_blank"
-          class="	fa fa-caret-square-o-down fa-lg"
-        >
-          {""}
-        </a>
       </p>
-      {/* <p class="credit_p">
-        inspired by{" "}
-        <a
-          href="https://www.tiffanywang.me/"
-          target="_blank"
-          class="credit"
-        >
-          tiffany
-        </a>
-      </p> */}
     </div>
   );
 }
